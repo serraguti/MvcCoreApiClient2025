acf8beab395f8962ccb2854681013b38bc5e7436
serraguti <pacoserranox@gmail.com>
2025-03-21 13:01:27 +0100

    commit

---
+++ MvcCoreApiClient/Controllers/HospitalesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcCoreApiClient.Controllers
+{
+    public class HospitalesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cliente()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcCoreApiClient/Services/ServiceHospitales.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Headers;
+
+namespace MvcCoreApiClient.Services
+{
+    public class ServiceHospitales
+    {
+        //NECESITAMOS LA URL DE ACCESO AL SERVICIO, SOLO LA URL
+        private string ApiUrl;
+        //NECESITAMOS INDICAR A NUESTRO SERVICE QUE LEEMOS 
+        //CODIGO JSON
+        private MediaTypeWithQualityHeaderValue header;
+
+        public ServiceHospitales()
+        {
+            this.ApiUrl = "https://apicorehospitalespaco.azurewebsites.net/";
+            this.header = new MediaTypeWithQualityHeaderValue
+                ("application/json");
+        }
+
+    }
+}
